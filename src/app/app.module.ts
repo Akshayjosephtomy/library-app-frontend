9cadf9a744b8f1c78fe1ff00f7efdc6ed2e5ed09
Akshayjosephtomy <akshayjt007@gmail.com>
2022-10-19 23:08:32 +0530

    l

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { AddbookComponent } from './addbook/addbook.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    AddbookComponent
   ],
   imports: [
     BrowserModule,
