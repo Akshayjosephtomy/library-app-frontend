8fb367d2cfdf73222244d5ca280ce3dec1bb0d4c
Akshayjosephtomy <akshayjt007@gmail.com>
2022-10-20 15:07:33 +0530

    l

--- src/app/addbook/addbook.component.ts
+++ src/app/addbook/addbook.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-addbook',
@@ -7,7 +8,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddbookComponent implements OnInit {
 
-  constructor() { }
+  constructor(private myapi:ApiService) { }
+
+  status:boolean=false
+
+  name=""
+  author=""
+  pdate=""
+  price=""
+  readvalue=()=>{
+    let data={
+      "name":this.name,
+      "author":this.author,
+      "pdate":this.pdate,
+      "price":this.price
+    }
+    console.log(data)
+    this.myapi.addBook(data).subscribe(
+      (response)=>{
+        console.log(response)
+        
+      }
+    )
+    alert("book added successfully")
+    this.name=""
+    this.author=""
+    this.pdate=""
+    this.price=""
+    this.status=true
+  }
 
   ngOnInit(): void {
   }
